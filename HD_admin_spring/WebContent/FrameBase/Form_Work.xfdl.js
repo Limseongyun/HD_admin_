7d12d565b9d0605c398bbd82aafaf30a125f768e
Limseongyun <lsy9699@naver.com>
2020-07-01 07:04:36 +0900

    ㅋ

--- HD_admin_spring/WebContent/FrameBase/Form_Work.xfdl.js
+++ HD_admin_spring/WebContent/FrameBase/Form_Work.xfdl.js
@@ -25,6 +25,11 @@
             obj.set_text("this is test");
             this.addChild(obj.name, obj);
 
+            obj = new Button("Button01","268","68","98","41",null,null,null,null,null,null,this);
+            obj.set_taborder("1");
+            obj.set_text("lsyv1");
+            this.addChild(obj.name, obj);
+
             // Layout Functions
             //-- Default Layout : this
             obj = new Layout("default","Desktop_screen",1280,720,this,function(p){});
@@ -47,12 +52,18 @@
         	this.alert('this is test')
         };
 
+        this.Button01_onclick = function(obj,e)
+        {
+
+        };
+
         });
         
         // Regist UI Components Event
         this.on_initEvent = function()
         {
             this.Button00.addEventHandler("onclick",this.Button00_onclick,this);
+            this.Button01.addEventHandler("onclick",this.Button01_onclick,this);
         };
 
         this.loadIncludeScript("Form_Work.xfdl");
